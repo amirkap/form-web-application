809d4955b5c92b0d83aec293666b7ad7a93c296e
Amir Kaplan <amirkap7@gmail.com>
2023-08-30 20:15:47 +0300

    Switched hosting to Render and db server to PostgreSQL

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-var express = require('express');
-var mysql = require('mysql');
-var dotenv = require('dotenv');
+const express = require('express');
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
 dotenv.config();
 
 const BASE_PATH = __dirname + '/';
 const HTML_FILE_PATH = __dirname + '/index.html';
-const DB_TABLE_NAME = "heroku_aa9b39c5b3399b8.employees";
+//const DB_TABLE_NAME = "employees";
 const DB_CONNECTION_MSG = "Connected to DB!";
 
-var app = express();
+const app = express();
 app.use(express.static(BASE_PATH));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/', function(req,res){
-    res.sendFile(HTML_FILE_PATH);
+// PostgreSQL configuration
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT || 5432,
 });
-app.post('/', function(req, res){
-         var con = mysql.createConnection({
-             host: process.env.DB_HOST,
-             user: process.env.DB_USER,
-             password: process.env.DB_PASSWORD
-         });
-        console.log(DB_CONNECTION_MSG);
-        let query = getInsertQuery(req.body);
-        con.query(query, function (err, result){
-            if (err) throw err;
-            console.log(result);
-            console.log("1 Employee updated");
-            setTimeout(() => res.sendFile(HTML_FILE_PATH), 1000);  
-        });
-        con.end();       
+
+app.get('/', function (req, res) {
+  res.sendFile(HTML_FILE_PATH);
+});
+
+app.post('/', function (req, res) {
+  console.log(DB_CONNECTION_MSG);
+  const query = getInsertQuery(req.body);
+
+  // Use the 'pg' module for query execution
+  pool.query(query)
+    .then(result => {
+      console.log(result);
+      console.log("1 Employee updated");
+      setTimeout(() => res.sendFile(HTML_FILE_PATH), 1000);
+    })
+    .catch(err => {
+      throw err;
+    });
 });
 
 /**
@@ -39,21 +47,24 @@ app.post('/', function(req, res){
  * @param {*} requestBody
  * @returns  'INSERT' query string 
  */
-function getInsertQuery (requestBody) {
-    let headers = "idNumber, firstName, lastName, dateOfBirth, salary"
-        let formValues = {idNumber:requestBody.idNumber,
-                          firstName:`"${requestBody.firstName}"`,
-                          lastName:`"${requestBody.lastName}"`,
-                          dateOfBirth:`"${requestBody.dateOfBirth}"`,
-                          salary:requestBody.salary}
-        let valuesAsStr = Object.values(formValues).join(",");
-        return `INSERT INTO ${DB_TABLE_NAME} (${headers}) VALUES (${valuesAsStr}) 
-                     ON DUPLICATE KEY 
-                        UPDATE firstName=${formValues.firstName},
-                               lastName=${formValues.lastName},
-                               dateOfBirth=${formValues.dateOfBirth},
-                               salary=${formValues.salary}`;
+function getInsertQuery(requestBody) {
+  const headers = "idNumber, firstName, lastName, dateOfBirth, salary";
+  const formValues = {
+    idNumber: requestBody.idNumber,
+    firstName: requestBody.firstName,
+    lastName: requestBody.lastName,
+    dateOfBirth: requestBody.dateOfBirth,
+    salary: requestBody.salary,
+  };
+  const valuesAsStr = Object.values(formValues).map(value => `'${value}'`).join(",");
+  return `INSERT INTO ${DB_TABLE_NAME} (${headers}) VALUES (${valuesAsStr}) 
+           ON CONFLICT (idNumber)
+           DO UPDATE SET 
+              firstName=EXCLUDED.firstName,
+              lastName=EXCLUDED.lastName,
+              dateOfBirth=EXCLUDED.dateOfBirth,
+              salary=EXCLUDED.salary`;
 }
 
-var port = process.env.PORT || 80;
-var server = app.listen(port, ()=> console.log('Online!')); 
+const port = process.env.PORT || 80;
+const server = app.listen(port, () => console.log('Online!'));
